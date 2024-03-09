f46b3c2ac59be0a51ab74aa9f5674adb0b8031c2
Alan Joseph <alanjosephmec@gmail.com>
2024-03-09 16:33:48 -0600

    Support for 512 query run; reference string generation; renaming files

--- referenece_gen.cu
+++ referenece_gen.cu
@@ -17,8 +17,7 @@
   } while (0)
 
 #define BLOCK_SIZE 128 // TODO: change me?
-#define QUERY_BATCH 512
-#define QUERY_LENGTH 2000 // TODO: change me?
+#define QUERY_LENGTH 10000 // TODO: change me?
 #define ITEMS_PER_THREAD 2 // TODO: change me?
 #define MIN_RANDOM_VAL 58
 #define MAX_RANDOM_VAL 120
@@ -85,9 +84,7 @@ int main(int argc, char **argv) {
     srand(time(NULL));
     // wbArg_t args;
     float *hostNormalizerInput; // the reference/query string we want to normalize
-    float *hostBatchNormalizerInput;
     float *hostNormalizerOutput; // the result of normalizing the string
-    float *hostBatchNormalizerOutput;
     float *hostOutputSums; // summation of all items in input
     float *hostOutputSquaredSums; // sum of all squares in input (used for variance calculation)
 
@@ -102,7 +99,7 @@ int main(int argc, char **argv) {
     // generate a randomized query file
     wbTime_start(Generic, "Generating random query");
     wbGenerateParams_t params;
-    params.raw.rows   = QUERY_BATCH;
+    params.raw.rows   = 1;
     params.raw.cols   = QUERY_LENGTH;
     params.raw.minVal = MIN_RANDOM_VAL;
     params.raw.maxVal = MAX_RANDOM_VAL;
@@ -111,38 +108,28 @@ int main(int argc, char **argv) {
     // hardcode the timestamp here to retrieve a previously-generated file
     std::time_t current_timestamp = std::time(nullptr);
 
-    char *inputFileName = wbPath_join(wbDirectory_current(), "test-normalizer", current_timestamp, "query.raw");
-    char *outputFileName = wbPath_join(wbDirectory_current(), "test-normalizer", current_timestamp, "normalized_query.raw");
+    char *inputFileName = wbPath_join(wbDirectory_current(), "reference-string", "random-number-string.raw");
+    char *outputFileName = wbPath_join(wbDirectory_current(), "reference-string", "normalized-reference-string.raw");
     wbDataset_generate(inputFileName, wbExportKind_raw, params);
     wbTime_stop(Generic, "Generating random query");
-    wbLog(TRACE, "Creaated ", QUERY_BATCH ,"random querys of length ", QUERY_LENGTH, " at test-normalizer", current_timestamp, "query.raw");
-
 
     // import the file just generated
     wbTime_start(Generic, "Importing data and creating memory on host");
     int inputLength = QUERY_LENGTH;
-    int queryLength, numQuerys;
-    hostBatchNormalizerInput = (float *)wbImport(inputFileName, &numQuerys, &queryLength);
-    wbLog(TRACE, "Number of Queries : ", numQuerys);
-    wbLog(TRACE, "Size of each Querie : ", queryLength);
-   
+    hostNormalizerInput = (float *)wbImport(inputFileName, &inputLength);
 
-     
-    int numBlockSums = queryLength / (BLOCK_SIZE << 1);
-    if (queryLength % (BLOCK_SIZE << 1)) {
+    int numBlockSums = QUERY_LENGTH / (BLOCK_SIZE << 1);
+    if (QUERY_LENGTH % (BLOCK_SIZE << 1)) {
         numBlockSums++;
     }
     
-    int inputSizeBytes = queryLength * sizeof(float);
+    int inputSizeBytes = QUERY_LENGTH * sizeof(float);
     int blockSumsSizeBytes = numBlockSums * sizeof(float);
-    int batchSizeBytes = queryLength * numQuerys * sizeof(float);
 
     // allocate host memory
     hostOutputSums = (float *)malloc(blockSumsSizeBytes);
     hostOutputSquaredSums = (float *)malloc(blockSumsSizeBytes);
     hostNormalizerOutput = (float *)malloc(inputSizeBytes);
-    hostNormalizerInput = (float *)malloc(inputSizeBytes);
-    hostBatchNormalizerOutput = (float *)malloc(batchSizeBytes);
     wbTime_stop(Generic, "Importing data and creating memory on host");
     
     wbTime_start(GPU, "Allocating GPU memory.");
@@ -153,87 +140,78 @@ int main(int argc, char **argv) {
     wbCheck(hipMalloc(&deviceOutputSquaredSums, blockSumsSizeBytes));
     wbTime_stop(GPU, "Allocating GPU memory.");
 
-    //--Start processing each query
-    for(int currentQuery = 0; currentQuery < numQuerys; currentQuery++) {
-        for(int i = 0; i < queryLength; i++) {
-            hostNormalizerInput[i] = hostBatchNormalizerInput[(currentQuery * queryLength) +i];
-        }
-        wbTime_start(GPU, "Copying input memory to the GPU.");
-        // copy input to GPU
-        wbCheck(hipMemcpy(deviceNormalizerInput, hostNormalizerInput, inputSizeBytes, hipMemcpyHostToDevice));
-        wbTime_stop(GPU, "Copying input memory to the GPU.");
+    wbTime_start(GPU, "Copying input memory to the GPU.");
+    // copy input to GPU
+    wbCheck(hipMemcpy(deviceNormalizerInput, hostNormalizerInput, inputSizeBytes, hipMemcpyHostToDevice));
+    wbTime_stop(GPU, "Copying input memory to the GPU.");
 
-        // initialize grid/block dimensions for the kernel that calculates sums
-        dim3 gridDimensions_sums(numBlockSums, 1, 1);
-        dim3 blockDimensions_sums(BLOCK_SIZE, 1, 1);
+    // initialize grid/block dimensions for the kernel that calculates sums
+    dim3 gridDimensions_sums(numBlockSums, 1, 1);
+    dim3 blockDimensions_sums(BLOCK_SIZE, 1, 1);
 
-        wbTime_start(Compute, "Performing HIP computation");
-        // calculate all sums and squared sums
-        hipLaunchKernelGGL(
-            calculate_mean_and_variance, gridDimensions_sums, blockDimensions_sums, 0, 0, 
-            deviceNormalizerInput, deviceOutputSums, deviceOutputSquaredSums, inputLength
-        );
+    wbTime_start(Compute, "Performing HIP computation");
+    // calculate all sums and squared sums
+    hipLaunchKernelGGL(
+        calculate_mean_and_variance, gridDimensions_sums, blockDimensions_sums, 0, 0, 
+        deviceNormalizerInput, deviceOutputSums, deviceOutputSquaredSums, inputLength
+    );
 
-        hipDeviceSynchronize();
-        wbTime_stop(Compute, "Performing HIP computation");
-        
-        // copy output sums and squared sums back to host
-        wbTime_start(Copy, "Copying output memory to the CPU");
-        // Copy the GPU memory back to the CPU here
-        wbCheck(hipMemcpy(hostOutputSums, deviceOutputSums, blockSumsSizeBytes, hipMemcpyDeviceToHost));
-        wbCheck(hipMemcpy(hostOutputSquaredSums, deviceOutputSquaredSums, blockSumsSizeBytes, hipMemcpyDeviceToHost));
+    hipDeviceSynchronize();
+    wbTime_stop(Compute, "Performing HIP computation");
+    
+    // copy output sums and squared sums back to host
+    wbTime_start(Copy, "Copying output memory to the CPU");
+    // Copy the GPU memory back to the CPU here
+    wbCheck(hipMemcpy(hostOutputSums, deviceOutputSums, blockSumsSizeBytes, hipMemcpyDeviceToHost));
+    wbCheck(hipMemcpy(hostOutputSquaredSums, deviceOutputSquaredSums, blockSumsSizeBytes, hipMemcpyDeviceToHost));
 
-        wbTime_stop(Copy, "Copying output memory to the CPU");
+    wbTime_stop(Copy, "Copying output memory to the CPU");
 
-        // reduce output sums & squared sums on the host
-        for (unsigned int ii = 1; ii < numBlockSums; ii++) {
-            hostOutputSums[0] += hostOutputSums[ii];
-            hostOutputSquaredSums[0] += hostOutputSquaredSums[ii];
-        }
-        float mean = hostOutputSums[0] / queryLength;
-        float variance = hostOutputSquaredSums[0];
-        variance  = variance/queryLength- mean*mean;
-        // this is 1 / sigma since we need to divide by sigma elsewhere.
-        // Faster to multiply by this factor than to compute lots of division on the GPU
-        variance  = variance > 0 ? 1.0/std::sqrt(variance) : 1;
-
-        wbLog(TRACE, "mu: ", mean, ", sigma: ", 1 / variance);
-
-        // copy reduced sums to constant memory on the device
-        wbCheck(hipMemcpyToSymbol(mean_X, &mean, sizeof(float), 0, hipMemcpyHostToDevice));
-        wbCheck(hipMemcpyToSymbol(variance_Y, &variance, sizeof(float), 0, hipMemcpyHostToDevice));
-
-        // initialize grid/block dimensions for the kernel that calculates sums
-        int numNormalizeThreadsPerBlock = queryLength / (BLOCK_SIZE * ITEMS_PER_THREAD);
-        if (queryLength % (BLOCK_SIZE * ITEMS_PER_THREAD)) {
-            numNormalizeThreadsPerBlock++;
-        }
-        dim3 gridDimensions_normalize(numNormalizeThreadsPerBlock, 1, 1);
-        dim3 blockDimensions_normalize(BLOCK_SIZE, 1, 1);
-
-        wbTime_start(Compute, "Performing HIP computation");
-        // calculate all sums and squared sums
-        hipLaunchKernelGGL(
-            normalize_data, gridDimensions_normalize, blockDimensions_normalize, 0, 0, 
-            deviceNormalizerInput, deviceNormalizerOutput, inputLength
-        );
-
-        hipDeviceSynchronize();
-        wbTime_stop(Compute, "Performing HIP computation");
-
-        wbTime_start(Copy, "Copying data from the GPU");
-        // Copy the device normalized array back to the host 
-        wbCheck(hipMemcpy(hostNormalizerOutput, deviceNormalizerOutput, inputSizeBytes, hipMemcpyDeviceToHost));
-        wbTime_stop(Copy, "Copying data from the GPU");
-
-        for(int i = 0; i < queryLength; i++) {
-            hostBatchNormalizerOutput[(currentQuery * queryLength) + i] = hostNormalizerOutput[i];
-        }
+    // reduce output sums & squared sums on the host
+    for (unsigned int ii = 1; ii < numBlockSums; ii++) {
+        hostOutputSums[0] += hostOutputSums[ii];
+        hostOutputSquaredSums[0] += hostOutputSquaredSums[ii];
     }
-    // write output to raw file
-    wbExport(outputFileName, (wbReal_t *)hostBatchNormalizerOutput, numQuerys, queryLength);
+    float mean = hostOutputSums[0] / QUERY_LENGTH;
+    float variance = hostOutputSquaredSums[0];
+    variance  = variance/QUERY_LENGTH- mean*mean;
+     // this is 1 / sigma since we need to divide by sigma elsewhere.
+     // Faster to multiply by this factor than to compute lots of division on the GPU
+    variance  = variance > 0 ? 1.0/std::sqrt(variance) : 1;
 
-    wbLog(TRACE, "normalized ", queryLength, " values");
+    wbLog(TRACE, "mu: ", mean, ", sigma: ", 1 / variance);
+
+    // copy reduced sums to constant memory on the device
+    wbCheck(hipMemcpyToSymbol(mean_X, &mean, sizeof(float), 0, hipMemcpyHostToDevice));
+    wbCheck(hipMemcpyToSymbol(variance_Y, &variance, sizeof(float), 0, hipMemcpyHostToDevice));
+
+    // initialize grid/block dimensions for the kernel that calculates sums
+    int numNormalizeThreadsPerBlock = QUERY_LENGTH / (BLOCK_SIZE * ITEMS_PER_THREAD);
+    if (QUERY_LENGTH % (BLOCK_SIZE * ITEMS_PER_THREAD)) {
+        numNormalizeThreadsPerBlock++;
+    }
+    dim3 gridDimensions_normalize(numNormalizeThreadsPerBlock, 1, 1);
+    dim3 blockDimensions_normalize(BLOCK_SIZE, 1, 1);
+
+    wbTime_start(Compute, "Performing HIP computation");
+    // calculate all sums and squared sums
+    hipLaunchKernelGGL(
+        normalize_data, gridDimensions_normalize, blockDimensions_normalize, 0, 0, 
+        deviceNormalizerInput, deviceNormalizerOutput, inputLength
+    );
+
+    hipDeviceSynchronize();
+    wbTime_stop(Compute, "Performing HIP computation");
+
+    wbTime_start(Copy, "Copying data from the GPU");
+    // Copy the device normalized array back to the host 
+    wbCheck(hipMemcpy(hostNormalizerOutput, deviceNormalizerOutput, inputSizeBytes, hipMemcpyDeviceToHost));
+    wbTime_stop(Copy, "Copying data from the GPU");
+
+    // write output to raw file
+    wbExport(outputFileName, (wbReal_t *)hostNormalizerOutput, 1, QUERY_LENGTH);
+
+    wbLog(TRACE, "normalized ", QUERY_LENGTH, " values");
 
     // Free device memory
     hipFree(deviceNormalizerInput);
@@ -243,9 +221,7 @@ int main(int argc, char **argv) {
 
     // Free host memory
     free(hostNormalizerInput);
-    free(hostBatchNormalizerInput);
     free(hostNormalizerOutput);
-    free(hostBatchNormalizerOutput);
     free(hostOutputSums);
     free(hostOutputSquaredSums);
 
